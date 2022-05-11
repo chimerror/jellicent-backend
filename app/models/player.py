e437f1623982c7069befa6498844d7519b352dee
Jayce Mitchell <jayce.mitchell@adadevelopersacademy.org>
2022-05-11 10:47:38 -0700

    Renaming player table to players

--- app/models/player.py
+++ app/models/player.py
@@ -1,6 +1,7 @@
 from app import db
 
 class Player(db.Model):
+    __tablename__ = 'players'
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     user_name = db.Column(db.String)
     display_name = db.Column(db.String)
