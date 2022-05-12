4f07ea931b6da9f3d03b1bfa2a89a634ad2c54f3
Jayce Mitchell <jayce.mitchell@adadevelopersacademy.org>
2022-05-12 13:12:49 -0700

    Seeded tests for GET /players and GET /players/x

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from app import create_app, db
+from app.models.player import Player
 from flask.signals import request_finished
 
 @pytest.fixture
@@ -20,3 +21,16 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+@pytest.fixture
+def five_players(app):
+    players = [
+        Player(user_name = "jayce", display_name = "Jayce Mitchell"),
+        Player(user_name = "chimmy", display_name = "Chimerelda Error"),
+        Player(user_name = "ada", display_name = "Ada Gates"),
+        Player(user_name = "glitch", display_name = "Glitch"),
+        Player(user_name = "f0c5", display_name = "0xF0C5"),
+    ]
+    db.session.add_all(players)
+    db.session.commit()
+    return players
--- tests/test_player_routes.py
+++ tests/test_player_routes.py
@@ -1,6 +1,33 @@
-def test_get_all_players_with_no_records(client):
+def test_get_all_players_happy_path(client, five_players):
+    response = client.get("/players")
+    response_body = response.get_json()
+    expected_response = []
+    current_expected_id = 1
+    for player in five_players:
+        expected_response.append({
+            "id": current_expected_id,
+            "user_name": player.user_name,
+            "display_name": player.display_name,
+        })
+        current_expected_id = current_expected_id + 1
+
+    assert response.status_code == 200
+    assert response_body == expected_response
+
+def test_get_all_players_no_records(client):
     response = client.get("/players")
     response_body = response.get_json()
 
     assert response.status_code == 200
     assert response_body == []
+
+def test_get_player_by_id_happy_path(client, five_players):
+    response = client.get("/players/2")
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+    assert response_body == {
+        "id": 2,
+        "user_name": five_players[1].user_name,
+        "display_name": five_players[1].display_name
+    }
