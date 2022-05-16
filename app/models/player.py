d9f1714f8a118a7bba4614b3066e253586597e1a
Jayce Mitchell <jayce.mitchell@adadevelopersacademy.org>
2022-05-16 10:31:03 -0700

    Making players columns non-nullable

--- app/models/player.py
+++ app/models/player.py
@@ -3,5 +3,5 @@ from app import db
 class Player(db.Model):
     __tablename__ = 'players'
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-    user_name = db.Column(db.String)
-    display_name = db.Column(db.String)
+    user_name = db.Column(db.String, nullable = False)
+    display_name = db.Column(db.String, nullable = False)
