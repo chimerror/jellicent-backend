7932f70fdde09f074ff34bb1bff15a918e1d6c34
Jayce Mitchell <jayce.mitchell@adadevelopersacademy.org>
2022-05-23 12:22:01 -0700

    Adding card count helper methods to GamePlayer

--- app/models/game_player.py
+++ app/models/game_player.py
@@ -23,3 +23,47 @@ class GamePlayer(db.Model):
     monkey_count = db.Column(db.SmallInteger, nullable = False, default = 0)
     chicken_count = db.Column(db.SmallInteger, nullable = False, default = 0)
     dog_count = db.Column(db.SmallInteger, nullable = False, default = 0)
+
+    def get_count_by_card_type(self, card_type):
+        if card_type == CardType.RAT:
+            return self.rat_count
+        elif card_type == CardType.RABBIT:
+            return self.rabbit_count
+        elif card_type == CardType.SNAKE:
+            return self.snake_count
+        elif card_type == CardType.SHEEP:
+            return self.sheep_count
+        elif card_type == CardType.MONKEY:
+            return self.monkey_count
+        elif card_type == CardType.CHICKEN:
+            return self.chicken_count
+        elif card_type == CardType.DOG:
+            return self.dog_count
+        elif card_type == CardType.WILD:
+            return self.wild_count
+        elif card_type == CardType.PLUS_TWO:
+            return self.plus_two_count
+        else:
+            raise ValueError(f"Unknown card type {card_type} passed to get_count_by_card_type")
+
+    def set_count_by_card_type(self, card_type, new_count):
+        if card_type == CardType.RAT:
+            self.rat_count = new_count
+        elif card_type == CardType.RABBIT:
+            self.rabbit_count = new_count
+        elif card_type == CardType.SNAKE:
+            self.snake_count = new_count
+        elif card_type == CardType.SHEEP:
+            self.sheep_count = new_count
+        elif card_type == CardType.MONKEY:
+            self.monkey_count = new_count
+        elif card_type == CardType.CHICKEN:
+            self.chicken_count = new_count
+        elif card_type == CardType.DOG:
+            self.dog_count = new_count
+        elif card_type == CardType.WILD:
+            self.wild_count = new_count
+        elif card_type == CardType.PLUS_TWO:
+            self.plus_two_count = new_count
+        else:
+            raise ValueError(f"Unknown card type {card_type} passed to set_count_by_card_type")
