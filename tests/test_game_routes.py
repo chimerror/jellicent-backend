bd121f7c86978629fd56cc95dbe8fe6c5dc9562f
Jayce Mitchell <jayce.mitchell@adadevelopersacademy.org>
2022-05-26 14:09:19 -0700

    Adding seeding test for POST /games

--- tests/test_game_routes.py
+++ tests/test_game_routes.py
@@ -1,4 +1,5 @@
 from app.models.card_type import PLAYER_CARDS
+from app.models.game import validate_game_id
 from app.models.game_status import GameStatus
 import re
 
@@ -95,6 +96,54 @@ def test_create_game_happy_path_five_players(client, five_players):
     assert get_response_body["piles"] == [[], [], [], [], []]
 
     validate_response_body_players(get_response_body, EXPECTED_PLAYER_IDS)
+
+def test_create_game_happy_path_seeding(client, five_players):
+    EXPECTED_PLAYER_IDS = [2, 3, 4]
+    post1_response = client.post("/games", json = {
+        "player-ids": EXPECTED_PLAYER_IDS,
+        "use-advanced-scoring": False,
+        "assign-wilds-on-take": False,
+        "random-seed": 13
+    })
+    post1_response_body = post1_response.text
+    post1_response_match = CREATE_RESPONSE_REGEX.match(post1_response_body)
+    game1_id =  post1_response_match.group("game_id")
+    assert post1_response.status_code == 201
+
+    post2_response = client.post("/games", json = {
+        "player-ids": EXPECTED_PLAYER_IDS,
+        "use-advanced-scoring": False,
+        "assign-wilds-on-take": False,
+        "random-seed": 13
+    })
+    post2_response_body = post2_response.text
+    post2_response_match = CREATE_RESPONSE_REGEX.match(post2_response_body)
+    game2_id =  post2_response_match.group("game_id")
+    assert post2_response.status_code == 201
+
+    get1_response = client.get(f"/games/{game1_id}")
+    get1_response_body = get1_response.get_json()
+    assert get1_response.status_code == 200
+    get2_response = client.get(f"/games/{game2_id}")
+    get2_response_body = get2_response.get_json()
+    assert get2_response.status_code == 200
+
+    assert get1_response_body["use-advanced-scoring"] == \
+        get2_response_body["use-advanced-scoring"]
+    assert get1_response_body["assign-wilds-on-take"] == \
+        get2_response_body["assign-wilds-on-take"]
+    assert get1_response_body["cards-left"] == get2_response_body["cards-left"]
+    assert get1_response_body["current-state"] == \
+        get2_response_body["current-state"]
+    assert get1_response_body["last-round"] == get2_response_body["last-round"]
+    assert get1_response_body["piles"] == get2_response_body["piles"]
+    assert get1_response_body["removed-card"] == \
+        get2_response_body["removed-card"]
+    assert get1_response_body["players"] == get2_response_body["players"]
+
+    game1 = validate_game_id(game1_id)
+    game2 = validate_game_id(game2_id)
+    assert game1.deck == game2.deck
 
 def validate_response_body_players(
     response_body, expected_player_ids, removed_card = None):
